15ea4c2402bcf73732a40a2696e7765ab2df373b
Leonard Lamprecht <mindrun@icloud.com>
2016-01-25 13:48:37 +0100

    Binary for building stuff

--- index.js
+++ index.js
@@ -4,5 +4,12 @@ const app = require('commander')
 
 app
   .version(require(__dirname + '/package.json').version)
+
+app
   .command('serve', 'Serve your site locally')
-  .parse(process.argv)
+  .option('-p, --port <port>', 'The port on which your site will be available')
+
+app
+  .command('build', 'Buily your site')
+
+app.parse(process.argv)
