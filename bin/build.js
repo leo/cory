4c6358be532d68809d256537bc97b55faf4b67a1
Leonard Lamprecht <mindrun@icloud.com>
2016-01-25 15:18:05 +0100

    Build sample pages

--- bin/build.js
+++ bin/build.js
@@ -1,1 +1,38 @@
 #!/usr/bin/env node
+
+const fs = require('fs')
+const path = require('path')
+
+const handlebars = require('handlebars')
+const workingDir = process.cwd()
+const output = workingDir + '/dist'
+
+const tags = {
+  greeting: 'Hello!'
+}
+
+try {
+  const files = fs.readdirSync(workingDir + '/pages')
+} catch (err) {
+  throw err
+}
+
+files.forEach(function (file) {
+  var meta = path.parse(workingDir + '/pages/' + file)
+  var fullPath = meta.dir + '/' + meta.base
+
+  fs.readFile(fullPath, 'utf8', function (err, content) {
+    if (err) {
+      throw err
+    }
+
+    var template = handlebars.compile(content)
+    var outputPath = output + '/' + meta.name + '.html'
+
+    try {
+      fs.writeFileSync(outputPath, template(tags))
+    } catch(err) {
+      throw err
+    }
+  })
+})
