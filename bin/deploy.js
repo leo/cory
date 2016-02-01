1871c115150cf813cc43353ffe3a124f47a4eb39
Leonard Lamprecht <mindrun@icloud.com>
2016-02-01 14:12:29 +0100

    Copy site output to gh-pages branch

--- bin/deploy.js
+++ bin/deploy.js
@@ -2,16 +2,24 @@
 
 const colors = require('colors')
 const git = require('nodegit')
-const exists = require('../lib/etc').isSite
+const etc = require('../lib/etc')
+const exec = require('child_process').execSync;
+const walk = require('walk')
+const makeDir = require('mkdirp').sync
+
+const path = require('path')
+const fs = require('fs')
 
 const Branch = git.Branch
 const Repository = git.Repository
 const Index = git.Index
 
-if (!exists()) {
+if (!etc.isSite()) {
   console.error('No site in here!'.red)
   process.exit(1)
 }
+
+Repository.open(process.cwd())
 
 const found = new Promise(function (resolve, reject) {
   Repository.open(process.cwd()).then(function (repo) {
@@ -34,8 +42,57 @@ const found = new Promise(function (resolve, reject) {
 })
 
 found.then(function (repo, branch) {
-  console.log('Yeah, branch exists!')
-  process.exit(1)
+
+  const walker = walk.walk(process.cwd() + '/dist', {
+    filters: ['.git']
+  })
+
+  var output = []
+
+  walker.on('file', function (root, fileStat, next) {
+    if (fileStat.name === '.DS_Store') {
+      return next()
+    }
+
+    const paths = {}
+
+    paths.full = path.resolve(root, fileStat.name)
+    paths.relative = paths.full.replace(process.cwd() + '/dist', '')
+
+    fs.readFile(paths.full, (err, data) => {
+      if (err) throw err
+
+      output.push({
+        path: process.cwd() + paths.relative,
+        buffer: data
+      })
+
+      next()
+    })
+  })
+
+  walker.on('end', function() {
+    try {
+      exec('git checkout gh-pages')
+    } catch (err) {
+      throw err
+    }
+
+    etc.deleteDir(process.cwd(), function() {}, true)
+
+    for (file of output) {
+      try {
+        makeDir(path.dirname(file.path))
+        fs.writeFileSync(file.path, file.buffer)
+      } catch (err) {
+        throw err
+      }
+    }
+
+    process.exit(1)
+
+  })
+
 })
 
 found.catch(function (reason) {
