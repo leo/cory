0d659e6db238e23f2ef3df48602a4e642e57d8ba
Leo <leo@users.noreply.github.com>
2016-03-02 17:52:58 +0100

    Support for update notifier
    This resolves #13

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
 
 const app = require('commander')
+const updateNotifier = require('update-notifier')
+const pkg = require(__dirname + '/package.json')
+
+updateNotifier({ pkg }).notify()
 
 app
   .version(require(__dirname + '/package.json').version)
