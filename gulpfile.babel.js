d4cdc69019546f700feebadcc5cdf91cde3c3a27
Leonard Lamprecht <mindrun@icloud.com>
2016-06-12 20:46:12 +0200

    Slimmer gulpfile

---
+++ gulpfile.babel.js
@@ -0,0 +1,30 @@
+import gulp from 'gulp'
+import babel from 'gulp-babel'
+import cache from 'gulp-cached'
+import { crop as cropExt } from 'gulp-ext'
+
+const paths = {
+  bin: 'bin/*',
+  lib: 'lib/**/*'
+}
+
+gulp.task('lib', () =>
+  gulp.src(paths.lib)
+  .pipe(cache('lib'))
+  .pipe(babel())
+  .pipe(gulp.dest('dist/lib')))
+
+gulp.task('bin', () =>
+  gulp.src(paths.bin)
+  .pipe(cache('bin'))
+  .pipe(babel())
+  .pipe(cropExt())
+  .pipe(gulp.dest('dist/bin')))
+
+gulp.task('watch', () => {
+  gulp.watch(paths.lib, ['lib'])
+  gulp.watch(paths.bin, ['bin'])
+})
+
+gulp.task('transpile', ['bin', 'lib'])
+gulp.task('default', ['watch', 'transpile'])
